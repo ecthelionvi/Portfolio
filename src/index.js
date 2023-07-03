1090a47de9cf6052d4187610e7743ce70605ae88
ecthelionvi <rsears2@huskers.unl.edu>
2023-07-03 15:42:51 -0500

    Refactoring

--- src/index.js
+++ src/index.js
@@ -70,16 +70,17 @@ document.addEventListener("keydown", backspace);
 document.addEventListener("keypress", key);
 
 const COMMANDS = {
-  help: 'Supported Commands: ["<span class="code">about</span>", "<span class="code">experience</span>", "<span class="code">education</span>", "<span class="code">skills</span>", "<span class="code">contact</span>"]',
+  help: 'Supported Commands: ["<span class="code">about</span>", "<span class="code">experience</span>", "<span class="code">education</span>", "<span class="code">projects</span>", "<span class="code">skills</span>", "<span class="code">contact</span>"]',
   about:
     "Hello 👋<br>I'm Robert Sears. I'm an Army veteran turned computer science student who loves coding and web development. In my free time, I powerlift, cherish my moments with my cat, and contribute to the veteran community. I'm all about growing, connecting with others, and using tech to make a difference.",
+  projects: 'Projects: ["<a target="_blank" rel="noopener noreferrer" href="https://github.com/ecthelionvi/Portfolio-Website" class="social link">Portfolio</a>", "<a target="_blank" rel="noopener noreferrer" href="https://github.com/ecthelionvi/NeoComposer.nvim" class="social link">NeoComposer</a>"]',
   skills:
     '<span class="code">Languages:</span> HTML, CSS, Python, JavaScript<br><span class="code">Technologies:</span> Git, PostgreSQL, REST API\'s<br><span class="code">Frameworks:</span> React, Django',
   education:
-    "B.S. Computer Science - University of Nebraska-Lincoln",
+    "B.S. Computer Science - University of Nebraska-Lincoln - May 2025",
   experience:
     "I'm currently working as a Teaching Assistant at Code Platoon. I help provide comprehensive training to over 30 military veterans and spouses, focusing on Full Stack Development. I collaborate closely with other faculty members to improve the curriculum and enhance the in-class experience.",
   contact:
-    'You can contact me on any of the following links:<br>["<a target="_blank" rel="noopener noreferrer" href="https://github.com/ecthelionvi" class="social link">GitHub</a>", "<a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/robert-sears-" class="social link">LinkedIn</a>"]',
+    'You can contact me on any of the following links: ["<a target="_blank" rel="noopener noreferrer" href="https://github.com/ecthelionvi" class="social link">GitHub</a>", "<a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/robert-sears-" class="social link">LinkedIn</a>"]',
   merlin: "<span style='font-size: 2rem;'>🐈‍⬛</span>",
 };
